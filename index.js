126f77abf1af166a6d355d0d5941207bbf270e3b
Janhvi Singh <janhvi52@gmail.com>
2022-12-11 14:19:25 +0530

    added a preloader to the website

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+$(document).ready(function() {
+//Preloader
+preloaderFadeOutTime = 500;
+function hidePreloader() {
+var preloader = $('.spinner-wrapper');
+preloader.fadeOut(preloaderFadeOutTime);
+}
+hidePreloader();
+});
+
 function darkMode() {
     var element = document.body;
     element.className = "dark-mode";
