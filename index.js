bab1f0d5f23c281dc82484a076ea70fa2fa87b0d
Shubham Yadav <100077254+Shubham185y@users.noreply.github.com>
2022-12-12 23:14:17 +0530

    updated ui

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
 function darkMode() {
     var element = document.body;
     element.className = "dark-mode";
+    let bg = document.querySelector('.nav-list');
+    bg.style. background = 'linear-gradient(45deg, #efdbdb, #00838f)';
+
+   
   }
   function lightMode() {
     var element = document.body;
     element.className = "light-mode";
+    let bg = document.querySelector('.nav-list');
+    bg.style. background = 'linear-gradient(45deg, #efdbdb, #f97a7a)';
+
+   
   }
+
+  window.addEventListener('resize', function(){
+    addRequiredClass();
+})
+
+
+function addRequiredClass() {
+    if (window.innerWidth < 1210) {
+        document.body.classList.add('mobile')
+    } else {
+        document.body.classList.remove('mobile') 
+    }
+}
+
+window.onload = addRequiredClass
+let hamburger = document.querySelector('.hamburger')
+let mobileNav = document.querySelector('.nav-list')
+
+let bars = document.querySelectorAll('.hamburger div')
+
+let isActive = false
+
+hamburger.addEventListener('click', function() {
+    mobileNav.classList.toggle('open')
+    if(!isActive) {
+        bars[0].style.transform = 'rotate(45deg)'
+        bars[1].style.opacity = '0'
+        bars[2].style.transform = 'rotate(-45deg)'
+        isActive = true
+    } else {
+        bars[0].style.transform = 'rotate(0deg)'
+        bars[1].style.opacity = '1'
+        bars[2].style.transform = 'rotate(0deg)'
+        isActive = false
+    }
+    
+
+})
