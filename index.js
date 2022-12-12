89cd89ff0eb4f50ccfd0fb4206a63dea6508d1a1
Rohit <rohitmondall8000@gmail.com>
2022-12-12 20:58:39 +0530

    Changed the UI of the website using simple css and changed some HTML.

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-$(document).ready(function() {
-//Preloader
-preloaderFadeOutTime = 500;
-function hidePreloader() {
-var preloader = $('.spinner-wrapper');
-preloader.fadeOut(preloaderFadeOutTime);
-}
-hidePreloader();
+$(document).ready(function () {
+  //Preloader
+  preloaderFadeOutTime = 500;
+  function hidePreloader() {
+    var preloader = $('.spinner-wrapper');
+    preloader.fadeOut(preloaderFadeOutTime);
+  }
+  hidePreloader();
 });
 
 function darkMode() {
-    var element = document.body;
-    element.className = "dark-mode";
-  }
-  function lightMode() {
-    var element = document.body;
-    element.className = "light-mode";
-  }
+  var element = document.body;
+  element.className = "dark-mode";
+}
+function lightMode() {
+  var element = document.body;
+  element.className = "light-mode";
+}
